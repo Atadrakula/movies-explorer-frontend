0bc653decffab90af4998f9b4747070fb2af245c
Atadrakula <120248683+Atadrakula@users.noreply.github.com>
2023-09-06 18:43:36 +0300

    WIP: feat: initial infra

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  // Определяем окружение, для которого будут применяться правила ESLint.
+  env: {
+    browser: true, // Для браузерного окружения.
+    commonjs: true, // Для среды CommonJS (Node.js).
+    es2021: true, // Для поддержки ECMAScript 2021.
+  },
+  // Используем настройки из расширения "airbnb-base".
+  extends: 'react-app',
+  // Определяем параметры парсера JavaScript.
+  parserOptions: {
+    ecmaVersion: 'latest', // Используйте необходимую версию ECMAScript.
+    sourceType: 'module', // Если вы используете модульную систему (например, ES modules).
+  },
+  // Определяем правила ESLint.
+  rules: {
+    // Разрешить использование глобальных переменных, которые часто используются в браузере.
+    'no-undef': 'error',
+    // Запретить неиспользуемые переменные.
+    'no-unused-vars': 'error',
+    // Запретить использование функций, не имеющих return.
+    'consistent-return': 'error',
+    // Запретить использование функций, которые могут быть определены позже в коде (hoisting).
+    'no-use-before-define': [
+      'error',
+      { functions: false, classes: true, variables: true },
+    ],
+    // Запретить использование alert, confirm и prompt в браузерном коде.
+    'no-alert': 'error',
+  },
+  settings: {
+    react: {
+      pragma: 'React', // Позволяет ESLint распознавать JSX синтаксис в файлах .js.
+    },
+  },
+};
---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  rules: {
+    indentation: 2, // Индентация в 2 пробела.
+    'max-empty-lines': 2, // Максимально допустимое количество пустых строк (2).
+    'color-hex-case': 'lower', // Шестнадцатеричные цвета записаны в нижнем регистре.
+    'declaration-block-trailing-semicolon': 'always', // Всегда добавлять точку с запятой в конце блока объявления.
+    'selector-pseudo-element-colon-notation': 'double', // Использовать два двоеточия для псевдоэлементов.
+    'property-no-unknown': true, // Запрещать неизвестные свойства.
+  },
+};
